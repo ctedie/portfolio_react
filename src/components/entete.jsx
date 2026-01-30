2b7e31904648a7a55dba4729dee14780a72edb2b
Cédric <cedric.tedie@gmail.com>
2026-01-30 22:35:56 +0100

    Placement des liens de navigation à droite

--- src/components/entete.jsx
+++ src/components/entete.jsx
@@ -8,15 +8,15 @@ function Header()
 {
     return (
         <>
-            <nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
+            <nav className="navbar navbar-dark navbar-expand-lg bg-dark fixed-top">
                 <Container>
                     <div className="navbar-brand" href="/">JOHN DOE</div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ">
                             <a className="nav-link active" aria-current="page" href="#">HOME</a>
                             <a className="nav-link" href="#">SERVICES</a>
                             <a className="nav-link" href="#">PORTFOLIO</a>
