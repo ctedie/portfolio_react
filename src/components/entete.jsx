8491c29243a77cd2ee5505b69f8ae35c4bb00897
Cédric <cedric.tedie@gmail.com>
2026-01-31 09:17:35 +0100

    Mise en place de la section 'a propos'.

--- src/components/entete.jsx
+++ src/components/entete.jsx
@@ -1,6 +1,7 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { NavLink } from 'react-router-dom'
 import './entete.css'
 
@@ -31,3 +32,18 @@ function Header()
 }
 
 export default Header
+/**
+ *     <Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">MonApp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Accueil</Nav.Link>
+            <Nav.Link href="/about">À propos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+ */
