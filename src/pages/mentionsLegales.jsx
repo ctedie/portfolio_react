99e5dd7df6ff60656a1d408a5bd8c48147677247
Cédric <cedric.tedie@gmail.com>
2026-02-01 12:50:37 +0100

    Remplissage de la partie hebergeur de l'accordéon

--- src/pages/mentionsLegales.jsx
+++ src/pages/mentionsLegales.jsx
@@ -7,7 +7,7 @@ function MentionsLegales() {
     <>
       <h1>Mentions légales</h1>
       <div class="accordion col-lg-8" id="accordionExample">
-        <div class="accordion-item">
+        <div class="accordion-item"> {/* Éditeur du site */ }
           <h2 class="accordion-header" id="headingOne">
             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
               Editeur du site
@@ -26,19 +26,23 @@ function MentionsLegales() {
             </div>
           </div>
         </div>
-        <div class="accordion-item">
+        <div class="accordion-item"> {/* Hébergeur du site */ }
           <h2 class="accordion-header" id="headingTwo">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Accordion Item #2
+              Hébergeur
             </button>
           </h2>
           <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
             <div class="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              <div>
+                <h2 id='titre-hebergeur'>alwaysdata</h2>
+                <p>91 Rue Faubourge Saint-Honoré, 75009 Paris</p>
+                <a href="#">www.alwaysdata.com</a> {/* TODO : Ajouter le lien et l'icone*/ }
+              </div>
             </div>
           </div>
         </div>
-        <div class="accordion-item">
+        <div class="accordion-item"> {/* Crédits */ }
           <h2 class="accordion-header" id="headingThree">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
               Accordion Item #3
