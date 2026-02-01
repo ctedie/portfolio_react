abcdfc84aef89b4f3f6d8ac00822d78ea17cf426
Cédric <cedric.tedie@gmail.com>
2026-02-01 19:09:40 +0100

    Ajout des card bottom

--- src/components/ItemPortfolio.jsx
+++ src/components/ItemPortfolio.jsx
@@ -1,14 +1,20 @@
 import "./ItemPortfolio.css"
 
-function ItemPortfolio({ image, alt, title, Description }) 
+function ItemPortfolio({ image, alt, title, Description, textFooter }) 
 {
     return (
-        <div className='contour'>
-            <img src={image} alt={alt} />
-            <h3>{title}</h3>
-            <p>{Description}</p>
-            <button type="button" className="btn btn-primary">Voir le site</button>
-        </div>
+        
+            <div className="card" style={{ width: "18rem" }}>
+                <img src={image} alt={alt} className="card-img-top" />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{Description}</p>
+                        <a href="#" className="btn btn-primary ">Voir le site</a>
+                        <div className="card-footer">
+                            {textFooter}
+                        </div>
+                    </div>
+            </div>
     )
 }
 
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -19,28 +19,15 @@ function Portfolio() {
       <img id="banniere" src={baniere} alt="Bannière Portfolio" />
       {/* TODO: Ajouter le titre de la page (composant) */}
       <TitrePage titre="Portfolio" description="Voici quelques-unes de mes réalisations" />
-      <div className="portfolio col-md-6 col-lg-8">
-          <div>
-            <ItemPortfolio image={freshFood} alt="Fresh food" title="Fresh food" Description="Description de Fresh food" />
-          </div>
-          <div>
-            <ItemPortfolio image={restaurantAkira} alt="Restaurant Akira" title="Restaurant Akira" Description="Description de Restaurant Akira" />
-          </div>
-          <div>
-            <ItemPortfolio image={espaceBienEtre} alt="Espace bien-être" title="Espace bien-être" Description="Description de Espace bien-être" />
-          </div>
-          <div>
-            <ItemPortfolio image={seo} alt="SEO" title="SEO" Description="Description de SEO" />
-          </div>
-          <div>
-            <ItemPortfolio image={apiCreation} alt="Création d'une API" title="Création d'une API" Description="Description de Création d'une API" />
-          </div>
-          <div>
-            <ItemPortfolio image={siteMockup} alt="Maquette d'un site web" title="Maquette d'un site web" Description="Description de Maquette d'un site web" />
-          </div>
-
+      <div className="portfolio col-md-6 col-lg-8 space-around flex">
+            <ItemPortfolio image={freshFood} alt="Fresh food" title="Fresh food" Description="Description de Fresh food" textFooter="Site réalisé avec PHP et MySQL" />
+            <ItemPortfolio image={restaurantAkira} alt="Restaurant Akira" title="Restaurant Akira" Description="Description de Restaurant Akira" textFooter="Site réalisé avec WordPress" />
+            <ItemPortfolio image={espaceBienEtre} alt="Espace bien-être" title="Espace bien-être" Description="Description de Espace bien-être" textFooter="Site réalisé avec LARAVEL" />
+            <ItemPortfolio image={seo} alt="SEO" title="SEO" Description="Description de SEO" textFooter="Utilisation des outils SEO" />
+            <ItemPortfolio image={apiCreation} alt="Création d'une API" title="Création d'une API" Description="Description de Création d'une API" textFooter="PHP - SYMFONY" />
+            <ItemPortfolio image={siteMockup} alt="Maquette d'un site web" title="Maquette d'un site web" Description="Description de Maquette d'un site web" textFooter="Réalisé avec FIGMA"/>
+            
       </div>
-
     </>
   )
 }
