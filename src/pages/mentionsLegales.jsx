daf9a0d7cbaa4b10988a979896796c2f5f1467a9
Cédric <cedric.tedie@gmail.com>
2026-02-01 12:37:58 +0100

    Premier niveau de l'accordéon

--- src/pages/mentionsLegales.jsx
+++ src/pages/mentionsLegales.jsx
@@ -6,16 +6,23 @@ function MentionsLegales() {
   return (
     <>
       <h1>Mentions légales</h1>
-      <div class="accordion" id="accordionExample">
+      <div class="accordion col-lg-8" id="accordionExample">
         <div class="accordion-item">
           <h2 class="accordion-header" id="headingOne">
             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-              Accordion Item #1
+              Editeur du site
             </button>
           </h2>
           <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
             <div class="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              <div>
+                {/* TODO : Ajouter les icones */}
+                <h2>John Doe</h2>
+                <p>40 rue Laure Diebold</p>
+                <p>69009 Lyon, France</p>
+                <p>Téléphone : 06 12 34 56 78</p>
+                <p>Email : john.doe@example.com</p>
+              </div>
             </div>
           </div>
         </div>
