9495b64695623956b346ebf4945c24c106795ab8
Cédric <cedric.tedie@gmail.com>
2026-02-02 12:59:34 +0100

    Suppression de commentaires inutiles

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -55,10 +55,3 @@ function Home() {
 }
 
 export default Home
-
-/**
- *           <div className="progress">
-            <div className="progress-bar" role="progressbar" aria-label="Basic example" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-
- */
