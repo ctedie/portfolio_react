e11b83b1e4ef2733c28c41e2664d5cc5ef066f2c
Cédric <cedric.tedie@gmail.com>
2026-02-02 12:58:11 +0100

    Photo John Doe => OK

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,39 +8,42 @@ function Home() {
   return (
     <>
 
-      <div className='w-100'><img src={imagePrincipale} alt="photo principale" /></div>
-      <div className='container border p-4 justify-content-between align-items-start gap-4 my-4 d-flex'>
-        <div className=' w-50'>
+      <div className='d-flex justify-content-center mb-5'>
+        <img className='h-75' src={imagePrincipale} alt="photo principale" />
+      </div>
+
+      <div className='container d-flex flex-column flex-md-row'>
+        <div className='container w-md-50'>
           <h2 className='border-bottom border-2 border-primary'>À propos</h2>
-          {/* <div ><img className='infos__photo' src={photoJohn} alt="photo de John Doe" /></div> */}
+          <div ><img className='img-fluid' src={photoJohn} alt="photo de John Doe" /></div>
           <p className='infos__texte'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea </p>
           <p className='infos__texte'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea </p>
           <p className='infos__texte'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea </p>
         </div>
-        <div className='competences w-50 d-flex flex-column' >
+        <div className='container w-md-50' >
           <h2 className='border-bottom border-2 border-primary'>Mes compétences</h2>
 
-          <p className='labelProgressBar'>HTML5 90%</p>
+          <p className='labelProgressBar mt-3 mb-1'>HTML5 90%</p>
           <div className="progress">
             <div className="progress-bar bg-danger" role="progressbar" aria-label="HTML5 90%" style={{ width: "90%" }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
 
-          <p className='labelProgressBar mt-1 mt-0'>CSS 80%</p>
+          <p className='labelProgressBar mt-3 mb-1'>CSS 80%</p>
           <div className="progress mt-0">
             <div className="progress-bar bg-info mt-0" role="progressbar" aria-label="CSS 80%" style={{ width: "80%" }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
 
-          <p className='labelProgressBar'>JavaScript 70%</p>
+          <p className='labelProgressBar mt-3 mb-1'>JavaScript 70%</p>
           <div className="progress">
             <div className="progress-bar bg-warning" role="progressbar" aria-label="JavaScript 70%" style={{ width: "70%" }} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
 
-          <p className='labelProgressBar'>PHP 60%</p>
+          <p className='labelProgressBar mt-3 mb-1'>PHP 60%</p>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="PHP 60%" style={{ width: "60%" }} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
 
-          <p className='labelProgressBar'>React 50%</p>
+          <p className='labelProgressBar mt-3 mb-1'>React 50%</p>
           <div className="progress">
             <div className="progress-bar bg-primary" role="progressbar" aria-label="React 50%" style={{ width: "50%" }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
