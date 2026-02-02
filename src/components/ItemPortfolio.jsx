3e7eb582aa85aed8d4dc24b2dcb488670d235efe
Cédric <cedric.tedie@gmail.com>
2026-02-02 18:29:40 +0100

    centrage du bouton des cartes

--- src/components/ItemPortfolio.jsx
+++ src/components/ItemPortfolio.jsx
@@ -4,17 +4,18 @@ function ItemPortfolio({ image, alt, title, Description, textFooter })
 {
     return (
         
-            <div className="card mb-4" style={{ width: "18rem" }}>
-                <img src={image} alt={alt} className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{Description}</p>
-                        <a href="#" className="btn btn-primary ">Voir le site</a>
-                        <div className="card-footer">
-                            {textFooter}
-                        </div>
-                    </div>
+        <div className="card mb-4" style={{ width: "20rem" }}>
+            <img src={image} alt={alt} className="card-img-top" />
+            <div className="card-body justify-content-center d-flex flex-column align-items-center">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{Description}</p>
+                <a href="#" className="btn btn-primary ">Voir le site</a>
             </div>
+            <div className="card-footer justify-content-center d-flex align-items-center">
+                {textFooter}
+            </div>
+
+        </div>
     )
 }
 
