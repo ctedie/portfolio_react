d96bb1d4b7e674176019f336263c251525dbf75e
Cédric <cedric.tedie@gmail.com>
2026-02-03 12:17:53 +0100

    petite modification du component TitrePage : w-75

--- src/components/TitrePage.jsx
+++ src/components/TitrePage.jsx
@@ -1,7 +1,7 @@
 function TitrePage({ titre, description }) 
 {
   return (
-    <div className="container titre-page-container text-center my-4 border-primary border-3 border-bottom pb-3 w-50 border-primary">
+    <div className="container titre-page-container text-center my-4 border-primary border-3 border-bottom pb-3 w-75 border-primary">
       <h1 className="titre-page fw-bold">{titre}</h1>
       <p className="description-titre">{description}</p>
     </div>
