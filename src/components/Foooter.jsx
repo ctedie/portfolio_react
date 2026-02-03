8707bc4bbe0266c478a3d01dcd5f9ce03c0df011
Cédric <cedric.tedie@gmail.com>
2026-02-03 21:03:35 +0100

    Mise en place correct des liens

--- src/components/Foooter.jsx
+++ src/components/Foooter.jsx
@@ -1,8 +1,8 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { NavLink } from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav'
-
 import { IoLogoGithub, IoLogoTwitter, IoLogoLinkedin } from "react-icons/io";
 
 function Footer()
@@ -20,34 +20,36 @@ function Footer()
               Tel : 06 12 34 56 78<br />
               john.doe@gmail.com<br />
             </p>
-            <a className="text-secondary me-3" href="https://github.com/JohnDoe" target="_blank" rel="noopener noreferrer"><IoLogoGithub size={24} /></a>
-            <a className="text-secondary me-3" href="https://twitter.com/JohnDoe" target="_blank" rel="noopener noreferrer"><IoLogoTwitter size={24} /></a>
-            <a className="text-secondary" href="https://linkedin.com/in/JohnDoe" target="_blank" rel="noopener noreferrer"><IoLogoLinkedin size={24} /></a>
+            <Nav className="flex-row justify-content-start">
+              <Nav.Link className="text-secondary me-3 p-0" href="https://github.com/JohnDoe" target="_blank" rel="noopener noreferrer"><IoLogoGithub size={24} /></Nav.Link>
+              <Nav.Link className="text-secondary me-3 p-0" href="https://twitter.com/JohnDoe" target="_blank" rel="noopener noreferrer"><IoLogoTwitter size={24} /></Nav.Link>
+              <Nav.Link className="text-secondary p-0" href="https://linkedin.com/in/JohnDoe" target="_blank" rel="noopener noreferrer"><IoLogoLinkedin size={24} /></Nav.Link>
+            </Nav>
           </Col>
 
           {/* Colonne 2 */}
-          <Col md={4} className='mt-3'>
+          <Col md={4} className='mt-3 navbar-nav'>
             <h5>Liens utiles</h5>
-            <Nav className="flex-column">
-              <Nav.Link href="/" className="text-light p-0 mb-1">Accueil</Nav.Link>
-              <Nav.Link href="/services" className="text-light p-0 mb-1">Services</Nav.Link>
-              <Nav.Link href="/portfolio" className="text-light p-0 mb-1">Portfolio</Nav.Link>
-              <Nav.Link href="/contacts" className="text-light p-0 mb-1">Me contacter</Nav.Link>
-              <Nav.Link href="/mentions-legales" className="text-light p-0 mb-1">Mentions légales</Nav.Link>
-            </Nav>
+            <ul className="navbar-nav">
+              <li className='nav-item'><NavLink to="/" className="nav-link p-0 mb-1">Accueil</NavLink></li>
+              <li className='nav-item'><NavLink to="/services" className="nav-link p-0 mb-1">Services</NavLink></li>
+              <li className='nav-item'><NavLink to="/portfolio" className="nav-link p-0 mb-1">Portfolio</NavLink></li>
+              <li className='nav-item'><NavLink to="/contacts" className="nav-link p-0 mb-1">Me contacter</NavLink></li>
+              <li className='nav-item'><NavLink to="/mentions-legales" className="nav-link p-0 mb-1">Mentions légales</NavLink></li>
+            </ul>
           </Col>
 
           {/* Colonne 3 */}
-          <Col md={4} className='mt-3'>
+          <Col md={4} className='mt-3 navbar-nav'>
             <h5>Mes dernières réalisations</h5>
-            <Nav className="flex-column">
-              <Nav.Link href="/portfolio" className="text-light p-0 mb-1">Fresh Food</Nav.Link>
-              <Nav.Link href="/portfolio" className="text-light p-0 mb-1">Restaurent Akira</Nav.Link>
-              <Nav.Link href="/portfolio" className="text-light p-0 mb-1">Espace bien-être</Nav.Link>
-              <Nav.Link href="/portfolio" className="text-light p-0 mb-1">SEO</Nav.Link>
-              <Nav.Link href="/portfolio" className="text-light p-0 mb-1">Création d'une API</Nav.Link>
-              <Nav.Link href="/portfolio" className="text-light p-0 mb-1">Maquette d'un site</Nav.Link>
-            </Nav>
+            <ul className="navbar-nav">
+              <li className='nav-item'><NavLink to="/portfolio" className="nav-link p-0 mb-1">Fresh Food</NavLink></li>
+              <li className='nav-item'><NavLink to="/portfolio" className="nav-link p-0 mb-1">Restaurent Akira</NavLink></li>
+              <li className='nav-item'><NavLink to="/portfolio" className="nav-link p-0 mb-1">Espace bien-être</NavLink></li>
+              <li className='nav-item'><NavLink to="/portfolio" className="nav-link p-0 mb-1">SEO</NavLink></li>
+              <li className='nav-item'><NavLink to="/portfolio" className="nav-link p-0 mb-1">Création d'une API</NavLink></li>
+              <li className='nav-item'><NavLink to="/portfolio" className="nav-link p-0 mb-1">Maquette d'un site</NavLink></li>
+            </ul>
           </Col>
         </Row>
       </Container>
