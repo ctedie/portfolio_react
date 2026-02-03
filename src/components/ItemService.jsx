c48e2c7638c8948e192341761fc9a8178162360f
Cédric <cedric.tedie@gmail.com>
2026-02-03 14:52:44 +0100

    Icones => OK

--- src/components/ItemService.jsx
+++ src/components/ItemService.jsx
@@ -1,15 +1,18 @@
+import { BsBrush } from "react-icons/bs";
+import { MdHeight } from "react-icons/md";
+import "./Itemservice.css";
 
 function ItemService({ logo, alt, title, Description }) 
 {
     return (
-        
-            <div className="card" style={{ width: "18rem" }}>
-                <img src={logo} alt={alt} className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title text-center fw-bold">{title}</h5>
-                        <p className="card-text text-center">{Description}</p>
-                    </div>
+
+        <div className="container card d-flex align-items-center" style={{ width: "18rem" }}>
+            <img src={logo} alt={alt} className="card-img-top pt-3" />
+            <div className="card-body">
+                <h5 className="card-title text-center fw-bold">{title}</h5>
+                <p className="card-text text-center">{Description}</p>
             </div>
+        </div>
     )
 }
 
